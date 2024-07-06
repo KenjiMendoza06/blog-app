2e5f87b44d4290731c84a348a9eb25ac3b7fa84c
Kenji Mendoza <140770300+KenjiMendoza06@users.noreply.github.com>
2024-07-06 15:49:10 +0800

    2nd commit

--- frontend/src/scenes/layout/index.jsx
+++ frontend/src/scenes/layout/index.jsx
@@ -1,27 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "../../components/Navbar";
-import Sidebar from "../../components/Sidebar"
+import Sidebar from "../../components/Sidebar";
+import { useGetUserQuery } from "../../state/api";
 
 export default function Layout() {
-    const isNonMobile = useMediaQuery('(min-width:600px)');
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    return (
-        <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
-            <Sidebar
-                isNonMobile={isNonMobile}
-                drawerWidth="250px"
-                isSidebarOpen={isSidebarOpen}
-                setIsSidebarOpen={setIsSidebarOpen} />
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
 
-            <Box>
-                <Navbar
-                    isSidebarOpen={isSidebarOpen}
-                    setIsSidebarOpen={setIsSidebarOpen} />
-                <Outlet />
-            </Box>
-        </Box>
-    )
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        user={data || {}}
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+
+      <Box flexGrow={1}>
+        <Navbar
+          user={data || {}}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
